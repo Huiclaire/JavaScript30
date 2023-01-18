04c2b643ac232ee1ee984fa556bdd3a5e0479fcf
Claire Lin <chunhuilin7516@gmail.com>
2023-01-18 19:59:03 +0800

    add handleProgress function

--- 11-Custom_Video_Player/script-Claire.js
+++ 11-Custom_Video_Player/script-Claire.js
@@ -2,7 +2,7 @@
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
-const progressBar = player.querySelector('.progree__filled');
+const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
@@ -33,10 +33,21 @@ function handleRangeUpdate() {
   video[this.name] = this.value;
 }
 
+// function handleProgess(){
+//   const percent = (video.currentTime / video.duration) * 100;
+//   progressBar.style.flexBasis = `${percent}%`;
+// }
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
 // Hook up the event listners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click',
